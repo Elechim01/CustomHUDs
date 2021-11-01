430ca11b6f0544ea4e22edb58dcf802ff589a65c
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-01 19:13:19 +0100

    Initial Commit

---
+++ CustomHUDs/CustomHUDsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHUDsApp.swift
+//  CustomHUDs
+//
+//  Created by Michele Manniello on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomHUDsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
